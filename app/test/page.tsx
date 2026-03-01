34edafbb592ecd689b85fdd31bbf450c90d5fe89
Ethan SMADJA <155528040+ethansmadjaa@users.noreply.github.com>
2026-03-01 21:49:36 +0100

    feat(test): add a test page for design components and functions

---
+++ app/test/page.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export default function Test() {
+    return <div>This is a test page to test design components or functions without affecting the main app. when adding something never delete this div and just replace everything with the tests just not this div and its content
+  </div>
+}
